5903be92508a8bb397cfb1bdac4dc968f38e658c
Ben Stehr <Mincho@Bens-MacBook-Pro.local>
2021-06-07 09:20:50 -0400

    complete game pattern

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var gamePattern = [];
-
-var randomNumber = Math.floor(Math.random() * 4);
-
+//function produces number from 0 to 3 and outputs random colour
 function nextSequence() {
 	var randomNumber = Math.floor(Math.random() * 4);
-	var randomChosenColour = buttonColours[randomNumber];
-	gamePattern.push(randomChosenColour);
+	var colours = ["red", "blue", "green", "yellow"];
+	return colours[randomNumber];
 }
+
+//random colour stored in randomChosenColour
+var randomChosenColour = nextSequence();
+
+//random colour pushed to end up buttonColours array
+var gamePattern = buttonColours.push(randomChosenColour);
