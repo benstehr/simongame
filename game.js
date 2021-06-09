9a4d3bb837fc450e47c67b69a7179d124d29a2e7
Ben Stehr <Mincho@Bens-MacBook-Pro.local>
2021-06-08 21:39:26 -0400

    complete step 3

--- game.js
+++ game.js
@@ -1,14 +1,26 @@
-var buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
 
-//function produces number from 0 to 3 and outputs random colour
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+//function produces number from 0 to 3
 function nextSequence() {
-	var randomNumber = Math.floor(Math.random() * 4);
-	var colours = ["red", "blue", "green", "yellow"];
-	return colours[randomNumber];
+	let randomNumber = Math.floor(Math.random() * 4);
+	return randomNumber;
 }
 
-//random colour stored in randomChosenColour
-var randomChosenColour = nextSequence();
+//random colour is produced
+let randomChosenColour = buttonColours[nextSequence()];
 
-//random colour pushed to end up buttonColours array
-var gamePattern = buttonColours.push(randomChosenColour);
+//push random colour to gamePattern array
+gamePattern.push(randomChosenColour);
+
+//select button with same id as randomChosenColour, add flash animation, add sound
+function buttonAnimation() {
+	let buttonSelected = document.getElementById(randomChosenColour);
+	buttonSelected.classList.add("black-button");
+	setTimeout(function () {
+		buttonSelected.classList.remove("black-button");
+	}, 100);
+	let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+	audio.play();
+}
